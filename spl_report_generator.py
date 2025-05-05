ac853a3b19bf1da64b06f4ed3265e5eaf2297ea0
noama-samreen <noama.samreen@coinbase.com>
2025-05-05 10:58:46 -0400

    Update spl_report_generator.py

--- spl_report_generator.py
+++ spl_report_generator.py
@@ -210,7 +210,7 @@ def create_pdf(token_data, output_dir):
         alignment=4,
         fontName='Helvetica'
     )
-    conflicts_text = """<b>Conflicts Certification:</b> To the best of your knowledge, please confirm that you and your immediate family: (1) have not invested more than $10,000 in the asset or its issuer, (2) do not own more than 1% of the asset outstanding, and (3) do not have a personal relationship with the issuer's management, governing body, or owners. For wrapped assets, the underlying asset must be considered for the purpose of this conflict certification, unless: 1) the asset is a stablecoin; or 2) has a market cap of over $100 billion dollars. For multi-chain assets every version of the multi-chain asset must be counted together for the purpose of this conflict certification."""
+    conflicts_text = """<b>Conflicts Certification:</b> To the best of your knowledge, please confirm that you and your immediate family: (1) have not invested more than $1,000 in the asset or its issuer, (2) do not own more than 1% of the asset outstanding, and (3) do not have a personal relationship with the issuer's management, governing body, or owners. For wrapped assets, the underlying asset must be considered for the purpose of this conflict certification, unless: 1) the asset is a stablecoin; or 2) has a market cap of over $100 billion dollars. For multi-chain assets every version of the multi-chain asset must be counted together for the purpose of this conflict certification."""
     elements.append(Paragraph(conflicts_text, conflicts_style))
     elements.append(Spacer(1, 10))
     
